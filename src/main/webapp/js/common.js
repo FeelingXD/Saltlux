3bc4212e1b03ea616efe2763a1e91e6601ba2314
Jimin <wlals425315@gmail.com>
2022-01-11 17:53:10 +0900

    logintest and make script file xD

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,3 +1,15 @@
 /**
  common.js
 **/
+function login_check(){
+    if(!document.login_form.id.value){
+        alert("id는 필수입력값입니다.");
+        document.login_form.id.focus();
+        return;
+    }
+    if(!document.login_form.pw.value){
+        alert("pw은 필수입력값입니다.");
+        document.login_form.pw.focus();
+    }
+    document.login_form.submit();
+}
