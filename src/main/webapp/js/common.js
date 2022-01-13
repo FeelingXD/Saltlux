c34346fdcc494ad78d6bfb8ab75834cdacd8920c
지민 <wlals425315@gmail.com>
2022-01-13 10:58:18 +0900

    update common.js for dbconn

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -11,14 +11,7 @@ function login_check(){
     if(!document.login_form.pw.value){
         alert("pw은 필수입력값입니다.");
         document.login_form.pw.focus();
-<<<<<<< HEAD
-<<<<<<< HEAD
-        return null;
-=======
->>>>>>> 3bc4212e1b03ea616efe2763a1e91e6601ba2314
-=======
-        return; 
->>>>>>> fce774bcc80a95b15a88d10b92a0dda4086c7185
+        return 
     }
     document.login_form.submit();
 }
