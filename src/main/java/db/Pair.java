caa904e3871d4e70cac2d3a5c1b76008c5cba2c4
지민 <wlals425315@gmail.com>
2022-01-14 13:03:02 +0900

    commit

--- src/main/java/db/Pair.java
+++ src/main/java/db/Pair.java
@@ -1,12 +1,12 @@
 package db;
 
 public class Pair<T>{
-	private T value;
-	private String type;
+	public T value;
+	public String type;
 	
-	public Pair<T>(T value1) {
-		this.value=value1;
-		this.type = value1.getClass().getSimpleName();
+	public Pair(T value) {
+		this.value =value;
+		this.type= value.getClass().getSimpleName();
 	}
 	
 	public T getValue(){
