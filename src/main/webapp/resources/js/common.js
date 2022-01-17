59680bbdfcf1c029e17494cb926f192e37501a4d
jimin <wlals425315@gmail.com>
2022-01-17 14:01:15 +0900

    update secure inject sql js

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -1,18 +1,26 @@
 /**
- * 
- */
+정규표현식
+이메일  "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/"
+휴대폰 "/^(010|011|016|017|018|019)-\d{3,4}-\d{4}$/u"
+**/
 
-function input_value_check(form_name){
+function input_value_check(form_name, expression){//form_name 폼이름, expression 표현식
 	const inputs = document.forms[form_name].elements;
-	for(let element of inputs){// 추후제작
-		//inputs[].value
+	var regexp = new RegExp(expression, 'g');
+	for(let element of inputs){//
+		if(String(element.value)!=""){// form 값이 비어있지않은경우 
+			return regexp.test(element.value);
+		}
 	}
-	
-
 }
 
 function check_input(form_name){// login TDD;
-	input_value_check(form_name);
+	
+	if(input_value_check(form_name,"/'|;|--|./"))
+	{
+		return alert('유효하지않은 접근입니다.');
+	} //inject secure
+	
 	
 	if(!document.forms[form_name].id.value){
 		alert("아이디값은 필수입니다.");
