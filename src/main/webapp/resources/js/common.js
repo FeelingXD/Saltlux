6e58e919e1d1ff513fb53e69213efef2aef33506
jimin <wlals425315@gmail.com>
2022-01-23 07:34:11 +0900

    Update common.js

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -5,8 +5,8 @@
 이름 "/^[가-힣]+$/"
 비밀번호 "/^[a-z]+[a-z0-9]$/g"
 **/
-const regArray=["/^[a-z0-9]+$/","/^[a-z0-9]$/","/^[가-힣]+$/","/^[a-z0-9]+$/","/[0-9a-zA-Z]([-_.]?[0-9a-zA-Z]+)*.[a-zA-Z]{2,3}$/"];  //정규식 배열 
-function input_value_check(form_name, expression){//폼 입력값 검증 form_name 폼이름, expression 표현식
+const regArray=[/^[a-z]+[a-z0-9]$/,/^[a-z0-9]+$/,/^[가-힣]{2,4}$/,/^[a-z0-9]+$/,/[0-9a-zA-Z]([-_.]?[0-9a-zA-Z]+)*.[a-zA-Z]{2,3}$/];  //정규식 배열 
+function input_value_check(form_name, expression="/'|;|--|./"){//폼 입력값 검증 form_name 폼이름, expression 표현식
 	const inputs = document.forms[form_name].elements;
 	var regexp = new RegExp(expression, 'g');
 	for(let element of inputs){
@@ -15,7 +15,7 @@ function input_value_check(form_name, expression){//폼 입력값 검증 form_na
 		}
 	}
 }
-function about_blank(form_name){
+function about_blank(form_name){ //raw
 	
 	const inputs = document.forms[form_name].elements;
 	for(element of inputs){
@@ -27,32 +27,79 @@ function about_blank(form_name){
 		}
 	}
 }
+
 function check_input(form_name){// login TDD
 
-	if(input_value_check(form_name,"/'|;|--|./"))
+	if(input_value_check(form_name))
 	{
 		return alert('유효하지않은 접근입니다.');
 	} //inject secure
 	
 	if(!document.forms[form_name].id.value){
+		
 		alert("아이디값은 필수입니다.");
-		document.forms[form_name].id.asdffocus();
-		return				
+		document.forms[form_name].id.focus();
+		return	;			
+		
 	}
 	if(!document.forms[form_name].pass.value){
+		
 		alert("비밀번호값은 필수입니다.");
 		document.forms[form_name].pass.focus();
-		return
+		return ;
+		
 	}
 	document.forms[form_name].submit();	
 }
 
 function check_input_memberform(form_name){//@only use in memberform aka sign-up
 
-	if(input_value_check(form_name,"/'|;|--|./"))
+	if(input_value_check(form_name))
 	{
+		
 		return alert('유효하지않은 접근입니다.');
+		
 	} //inject secure
-	about_blank(form_name);
 	
+	(function(form_name){
+		let i = 0;
+		let j = i;
+		const inputs = document.forms[form_name].elements;
+		
+		for(element of inputs){// blankfield check.
+			
+			if(element.value=="")
+			{
+				
+				alert(element.getAttribute('tag')+"값은 필수입니다.");
+				element.focus();
+				return ;
+				
+			}
+		}
+		
+		for(i =0 ; i < (inputs.length-1) ; i++,j++){ //value reg check.
+			
+			
+			if(i==2){
+				j= i-1;
+				continue;
+			}
+				
+			if(!regArray[j].test(inputs[i].value)){
+
+				return alert(inputs[i].getAttribute('tag')+'값을 확인해주세요');
+				
+			}
+			
+		}
+		
+		if(input.pass.value!=input.passconfirm.value){
+				
+				return alert('비밀번호가 일치하지않습니다.');
+				
+			}
+			
+	document.forms[form_name].submit();
+	})(form_name);
 }
